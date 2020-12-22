713b6611b6de4e93266546984e36a6d9f895d276
paukkle <paukkle@gmail.com>
2020-12-22 14:39:41 +0200

    1.a käyty loppuun

--- Osa1/part1/src/index.js
+++ Osa1/part1/src/index.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 20
 
+const Hello = (props) => {  //tiedonvälitys propsin avulla
   return (
     <div>
-      <p>Hello world, it is {now.toString()}</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <p>Hello {props.name}, you are {props.age} years old</p>
     </div>
+  )
+}
+
+const Footer = () => {
+  return (
+    <>
+      greeting app created by
+      <br />
+      <a href="https://github.com/paukkle/">paukkle</a>
+    </>
+  )
+}
+
+const App = () => {
+  const nimi = "Kärki"
+  const ika = 87
+  console.log("Pitäisi tulostua (Arvo, 18) ja (Kärki, 87)")
+  return ( //Alla tyhjä juurielementti <>
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Arvo" age={22 - 4}/>
+      <Hello name={nimi} age={ika}/>
+      <Footer />
+    </>
     )
   }
 
