2a048a39b2830b0f7c5c6b79edc9c20077109600
paukkle <paukkle@gmail.com>
2020-12-22 15:26:49 +0200

    1.1 valmis

--- Osa1/kurssitiedot/src/index.js
+++ Osa1/kurssitiedot/src/index.js
@@ -1,6 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root')
-);
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
 
+const Content = (props) => {
+  return (
+    <p>
+      {props.part} {props.exs}
+    </p>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.total}</p>
+  )
+}
+
+const App = () => {
+  const course = "Half Stack application development"
+  const part1 = "Fundamentals of React"
+  const exercises1 = 10
+  const part2 = "Using props to pass data"
+  const exercises2 = 7
+  const part3 = "State of a component"
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} exs={exercises1} />
+      <Content part={part2} exs={exercises2} />
+      <Content part={part3} exs={exercises3} />
+      <Total total={exercises1 + exercises2 + exercises3} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
