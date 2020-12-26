31f97eb34b50cc692d5cf2eb563b7c3355f62c0b
paukkle <paukkle@gmail.com>
2020-12-26 14:13:09 +0200

    1.7 valmis

--- Osa1/unicafe/src/index.js
+++ Osa1/unicafe/src/index.js
@@ -25,11 +25,36 @@ const ShowState = (props) => {
   )
 }
 
+const CountAll = (states) => states.reduce((a, b) => a + b, 0)
+
+const CountAve = (states) => {
+  const revSum = states[0] * 1 - states[2] * 1
+  const len = CountAll(states)
+  if (len === 0) {
+    return 0
+  }
+  else {
+    return revSum / len
+  }
+}
+
+const Positives = (states) => {
+  const len = CountAll(states)
+  if (len === 0) {
+    return 0
+  }
+  else {
+    return 100 * (states[0] / len)
+  }
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const states = [good, neutral, bad]
 
   const increaseByOne = ( setFun, state ) => {
     setFun(state + 1)
@@ -46,6 +71,15 @@ const App = () => {
       <ShowState text={"good"} state={good} />
       <ShowState text={"neutral"} state={neutral} />
       <ShowState text={"bad"} state={bad} />
+      <p>
+        all {CountAll(states)}
+      </p>
+      <p>
+        average {CountAve(states)}
+      </p>
+      <p>
+        positive {Positives(states)} %
+      </p>
     </div>
   )
 }
