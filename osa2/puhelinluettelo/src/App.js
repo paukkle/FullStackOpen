d40be2a84493419d9328b1ac9bd2eaf0b784b70d
paukkle <paukkle@gmail.com>
2021-01-02 14:33:43 +0200

    2.8

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,14 +2,17 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '040-1231244' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
     const nameObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     persons.map(person => person.name).includes(newName)
@@ -24,6 +27,10 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleSetNewNumber = (event) => {
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
@@ -35,11 +42,17 @@ const App = () => {
           />
         </div>
         <div>
+          number:
+          <input value={newNumber}
+          onChange={handleSetNewNumber}
+          />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <div key={person.name}>{person.name}</div>)}
+      {persons.map((person) => <div key={person.name}>{person.name} {person.number}</div>)}
     </div>
   )
 
