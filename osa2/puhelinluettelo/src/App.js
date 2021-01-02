2aa45c9bc506d4694106cc50f1c90fc8f734a028
paukkle <paukkle@gmail.com>
2021-01-02 17:15:49 +0200

    2.9

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,12 +1,23 @@
 import React, { useState } from 'react'
 
+const Persons = ({ persons, nameFilter} ) => {
+
+  if (nameFilter !== '') {
+    persons = persons.filter(person => person.name.toLowerCase().includes(nameFilter.toLowerCase()))
+  }
+  return persons.map(person => <div>{person.name} {person.number}</div>)
+}
+
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas',
-      number: '040-1231244' }
-  ]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  ])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
+  const [ newFilter, SetFilter ] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -19,21 +30,27 @@ const App = () => {
        ? alert(`${newName} is already added to phonebook`)
        : setPersons(persons.concat(nameObject))
          setNewName('')
-    
   }
 
-  const handleSetNewName = (event) => {
-    //console.log(event.target.value)
-    setNewName(event.target.value)
-  }
+  const handleSetNewName = (event) => setNewName(event.target.value)
+  
+  const handleSetNewNumber = (event) => setNewNumber(event.target.value)
+  
+  const handleFilter = (event) => SetFilter(event.target.value)
 
-  const handleSetNewNumber = (event) => {
-    setNewNumber(event.target.value)
-  }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <form>
+        <div>
+          filter shown with
+          <input value={newFilter}
+          onChange={handleFilter}
+          />
+        </div>
+      </form>
+      <h2>add a new</h2>
       <form onSubmit={addName}>
         <div>
           name: 
@@ -52,7 +69,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <div key={person.name}>{person.name} {person.number}</div>)}
+      <Persons persons={persons} nameFilter={newFilter} />
     </div>
   )
 
