f2a111f501e6ae2c79a7dbd93ac840e082b54e32
paukkle <paukkle@gmail.com>
2021-01-06 13:13:32 +0200

    2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Persons = ({ persons, nameFilter} ) => {
 
@@ -42,15 +43,21 @@ const Input = ({ value, handleValue, text }) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
+  
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
   const [ newFilter, SetFilter ] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios.get('http://localhost:3001/persons')
+    .then(response => {
+      console.log('fulfilled')
+      setPersons(response.data)
+    })
+  }, [])
+
 
   const addName = (event) => {
     event.preventDefault()
